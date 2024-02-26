beb1f7de4b285d82ac54b74ca75281a90db64790
whistle <435424527@qq.com>
2024-02-26 22:25:14 +0800

    202402262225

--- src/core/opSerialport.ts
+++ src/core/opSerialport.ts
@@ -1,4 +1,5 @@
 import { SerialPort, SerialPortOpenOptions } from "serialport";
+import {exec} from "child_process"; 
 import * as vscode from "vscode"
 
 let CF_COM = ""
@@ -12,26 +13,52 @@ export default {
     const quickPick = vscode.window.createQuickPick()
 		quickPick.items = portList.map(v => ({label: v}));
 		quickPick.onDidChangeSelection(e => {
-			console.log(e); 
+			console.log("onDidChangeSelection", e); 
 			CF_COM = e[0].label
+      vscode.window.showInformationMessage("设备连接成功")
 			quickPick.dispose()
 		})
 		quickPick.onDidHide(() => quickPick.dispose())
 		quickPick.show()
   },
-
+  // 串口监听
   listenSp: async () => {
     let portInfo = await SerialPort.list();
 
-    // 后更改，只保留创趣相关
+    //!!! 后更改，只保留创趣相关
     portInfo = portInfo.filter(v => v)
     console.log(portInfo)
 
     let portList = portInfo.map(v => v.path)
     console.log(portList)
+    console.log("CF_COM", CF_COM)
     if (CF_COM) {
 			if (portList.indexOf(CF_COM) >= 0) {
-				const sp = new SerialPort({path: CF_COM, baudRate: CF_BAUDRATE})
+        vscode.window.showInformationMessage("开始监听")
+        const ter = vscode.window.createTerminal(CF_COM)
+        ter.show(true)
+        ter.sendText(`terminal-s -p ${CF_COM} -b ${CF_BAUDRATE}`)
+        setTimeout(()=>{
+          ter.sendText(`\x03`)
+        }, 2000)
+
+
+				// const sp = new SerialPort({path: CF_COM, baudRate: CF_BAUDRATE}, err => {
+        //   if (!err) {
+              // sp.on('data', data => {
+              //   let str = data.toString()
+              //   // 非空格字符显示
+              //   if(!(/^\s*$/.test(str))) {
+              //     console.log('Data:', str)
+              //     ter.sendText(str, false)
+              //     // ter.
+              //   }
+              // })
+              // sp.on("close", ()=>vscode.window.showErrorMessage("设备已断开"))
+        //   }
+        //   else {console.log(err); vscode.window.showErrorMessage("设备连接异常")}
+        //   }
+        // )
 			} else {vscode.window.showErrorMessage("设备已断开")}
 		} else {vscode.window.showErrorMessage("请先连接串口")}
   }
--- src/core/setupPyb.ts
+++ src/core/setupPyb.ts
@@ -20,5 +20,26 @@ export default {
         }
       }
       });
+  },
+
+  // !!! 细化已安装的状态
+  installTerminalS: () => {
+    exec("pip install terminal-s", (error, stdout, stderr) => {
+      if (error) {
+        console.log(error)
+        vscode.window.showErrorMessage('脚本运行错误')
+        return;
+      } else if(stderr) {
+        vscode.window.showErrorMessage('repl调试工具安装失败')
+        return;
+      }
+      else {
+        const re = /Successfully\s*installed\s*terminal-s/
+        console.log(stdout)
+        if (re.test(stdout)) {
+          vscode.window.showInformationMessage('repl调试工具安装成功')
+        }
+      }
+    })
   }
 }
